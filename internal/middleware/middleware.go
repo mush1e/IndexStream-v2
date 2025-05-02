3b682a950bedd4d1815004bf2080631ba40705dc
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-05-02 13:17:07 -0400

    [task] improved logger middleware to display response status code and size

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -6,10 +6,37 @@ import (
 	"time"
 )
 
+type recorder struct {
+	http.ResponseWriter
+	status int
+	size   int
+}
+
+func (r *recorder) WriteHeader(code int) {
+	r.status = code
+	r.ResponseWriter.WriteHeader(code)
+}
+
+func (r *recorder) Write(b []byte) (int, error) {
+	if r.status == 0 {
+		r.status = http.StatusOK
+	}
+	n, err := r.ResponseWriter.Write(b)
+	r.size += n
+	return n, err
+}
+
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		next.ServeHTTP(w, r)
-		log.Println(r.Method, r.URL.Path, time.Since(start))
+		rec := &recorder{ResponseWriter: w}
+		next.ServeHTTP(rec, r)
+		log.Printf("%s | %s | %d | %dB | %v",
+			r.Method,
+			r.URL.RequestURI(),
+			rec.status,
+			rec.size,
+			time.Since(start),
+		)
 	})
 }
