e01e672ba52e3644e1b56fc1dba2bbff4b9eebf7
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-05-05 17:40:14 -0400

    [task] layed out scafolding for web Crawler need to look into concurrency + need to write crawl service

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IndexStream-v2
 Better index stream search engine this time going to make it WAYYYYYYYYY better
+
+
+= `TF-IDF` or `BM-26`
+- 
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
@@ -15,4 +16,23 @@ func GetSearch(w http.ResponseWriter, r *http.Request) {
 
 func GetCrawl(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Crawl endpoint coming soon!")
+}
+
+func PostCrawl(w http.ResponseWriter, r *http.Request) {
+	crawl_url := r.URL.Query().Get("url")
+
+	if crawl_url == "" {
+		http.Error(w, "invalid query: missing 'url' parameter", http.StatusBadRequest)
+		return
+	}
+
+	if u, err := url.ParseRequestURI(crawl_url); err != nil || u.Host == "" || (u.Scheme != "http" && u.Scheme != "https") {
+		http.Error(w, "bad URL provided", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("crawl has been queued for " + crawl_url))
+
+	// Implement crawl service
 }
