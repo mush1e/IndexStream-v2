99f53cf6b6ad2a546fb0e76ad2e60b6be3924f34
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-05-07 21:34:35 -0400

    [task] fetching HTML byte string successfully

--- internal/service/crawler.go
+++ internal/service/crawler.go
@@ -1,9 +1,32 @@
 package service
 
-import "log"
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
 func Crawl(url string) {
 	log.Printf("Starting crawl on %q\n", url)
-	// Do stuff
-	log.Printf("Finished crawling %q\n", url)
+	defer log.Printf("Finished crawling %q\n", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Failed to fetch %q\n\tnerr : %v\n", url, err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Non-OK status for %q : %v", url, resp.StatusCode)
+		return
+	}
+
+	body_content, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Printf("Error reading content of &q\n\terr : %v\n", url, err)
+	}
+
+	defer resp.Body.Close()
+
+	log.Printf("%v\n", string(body_content))
 }
