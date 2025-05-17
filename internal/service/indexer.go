8b8fc432fb65d0fa45e05ce7cd4657b4a96555b9
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-05-17 19:46:52 -0400

    [task] wrote inital indexer to setup our search functionality

--- internal/service/indexer.go
+++ internal/service/indexer.go
@@ -1,18 +1,56 @@
 package service
 
-import (
-	"log"
-	"sync"
-)
+import "sync"
 
 type Index struct {
-	mu    sync.RWMutex
-	index map[string]map[string]int64
+	mu        sync.RWMutex
+	index     map[string]map[string][]int
+	docLen    map[string]int
+	docFreq   map[string]int
+	docCount  int
+	avgDL     float64
+	sumDocLen int
 }
 
-func (i *Index) addDocument(docID string, terms []string) {
-	i.mu.Lock()
-	defer i.mu.Unlock()
-	// TODO populate our index
-	log.Println(docID, " : ", terms)
+func NewInvertedIndex() *Index {
+	return &Index{
+		index:   make(map[string]map[string][]int),
+		docLen:  make(map[string]int),
+		docFreq: make(map[string]int),
+	}
+}
+
+func (idx *Index) AddDocument(docID string, tokens []string) {
+	idx.mu.Lock()
+	defer idx.mu.Unlock()
+
+	// If document with docID already exists, do nothing
+	if _, found := idx.docLen[docID]; found {
+		return
+	}
+
+	idx.docCount++
+	idx.docLen[docID] = len(tokens)
+
+	// track which term we've already bumped document frequency for
+	seen := map[string]bool{}
+
+	for pos, token := range tokens {
+		// Init new term in index if needed
+		if idx.index[token] == nil {
+			idx.index[token] = make(map[string][]int)
+		}
+		idx.index[token][docID] = append(idx.index[token][docID], pos)
+
+		// bump doc frequency once for each term
+		if !seen[token] {
+			idx.docFreq[token]++
+			seen[token] = true
+		}
+	}
+
+	// recompute avg doc length
+	idx.sumDocLen += len(tokens)
+	idx.avgDL = float64(idx.sumDocLen) / float64(idx.docCount)
+
 }
